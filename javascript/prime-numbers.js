3e5a37757e06fb934d0e0b568b566e3287f5bea6
Camden Brown <camdenebrown@gmail.com>
2022-01-15 15:50:18 -0600

    prime number test problem

---
+++ javascript/prime-numbers.js
@@ -0,0 +1,11 @@
+primeNumberCheck = (num) => {
+  for(let i = 2; i < num; i++)
+    if(num % i === 0) return false;
+  return num > 1;
+}
+
+console.log(primeNumberCheck(0)) //false
+console.log(primeNumberCheck(1)) //false
+console.log(primeNumberCheck(2)) //true
+console.log(primeNumberCheck(825)) //true
+console.log(primeNumberCheck(827)) //false
