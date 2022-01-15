fd9adfd0fe89895dfa896dbb622bfe3fe8bbd8e9
Camden Brown <camdenebrown@gmail.com>
2022-01-15 16:24:10 -0600

    prime numbers up to n

--- javascript/prime-numbers.js
+++ javascript/prime-numbers.js
@@ -1,11 +1,25 @@
-primeNumberCheck = (num) => {
+primeNumber = (num) => {
   for(let i = 2; i < num; i++)
     if(num % i === 0) return false;
   return num > 1;
 }
 
-console.log(primeNumberCheck(0)) //false
-console.log(primeNumberCheck(1)) //false
-console.log(primeNumberCheck(2)) //true
-console.log(primeNumberCheck(825)) //false
-console.log(primeNumberCheck(827)) //true
+console.log(primeNumber(0)) //false
+console.log(primeNumber(1)) //false
+console.log(primeNumber(2)) //true
+console.log(primeNumber(825)) //false
+console.log(primeNumber(827)) //true
+
+primeNumbers = (num) => {
+  const arr = [];
+  let i = 2;
+  while (arr.length < num) {
+    if (primeNumber(i)) arr.push(i)
+    i++
+  }
+  return arr;
+}
+
+console.log(primeNumbers(2))
+console.log(primeNumbers(5))
+console.log(primeNumbers(10000)) // =)
